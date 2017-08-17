cfe6c5ea25a6ebde2321766a591c723e89937a63
BY-MINSK\AlexeyRozdyalovsky <alexeyrozdyalovsky@coherentsolutions.com>
2017-08-17 10:50:25 +0300

    initial

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var module = angular.module('myApp', []);
+
+module.controller('ParentCtrl', function ($scope){
+    $scope.text = "Hello world!";
+});
+
+module.controller('ChildCtrl', function ($scope){
+
+});
+//# sourceMappingURL=ls.directive.js.map
